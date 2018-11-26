c0bcc1e27eab7741b2e9ce51ae564360f4c8a60a
MarcosFerreiraLF <42723997+MarcosFerreiraLF@users.noreply.github.com>
2018-11-26 12:10:16 -0300

    Add files via upload
    Adição dos JavaBeans de cadastro de usuários

---
+++ Lodge-ejb/src/java/model/UsuarioFacadeLocal.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.List;
+import javax.ejb.Local;
+
+/**
+ *
+ * @author 201619030381
+ */
+@Local
+public interface UsuarioFacadeLocal {
+
+    void create(Usuario usuario);
+
+    void edit(Usuario usuario);
+
+    void remove(Usuario usuario);
+
+    Usuario find(Object id);
+
+    List<Usuario> findAll();
+
+    List<Usuario> findRange(int[] range);
+
+    int count();
+
+    public Usuario findByLogin(String login);
+    
+}
